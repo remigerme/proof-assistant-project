61ba6e83acd41f410e2c46b7d28e70def83ce399
remi.germe <remi.germe@gmail.com>
2024-11-24 18:52:26 +0100

    adding products (+ renaming Arr to TAbs)

--- simple/prover.ml
+++ simple/prover.ml
@@ -1,20 +1,28 @@
 let () = Printexc.record_backtrace true
 
-(* Type variables. *)
+(* Type variables *)
 type tvar = string
 
-(* Term variables. *)
+(* Term variables *)
 type var = string
 
-(** Types. *)
-type ty = TVar of tvar | Arr of ty * ty
+(* Types *)
+type ty = TVar of tvar | TAbs of ty * ty | TProd of ty * ty
 
-type tm = Var of var | App of tm * tm | Abs of var * ty * tm
+(* Terms *)
+type tm =
+  | Var of var
+  | App of tm * tm
+  | Abs of var * ty * tm
+  | Prod of tm * tm
+  | Fst of tm
+  | Snd of tm
 
 let rec string_of_ty t =
   match t with
   | TVar x -> x
-  | Arr (u, v) -> "(" ^ string_of_ty u ^ " => " ^ string_of_ty v ^ ")"
+  | TAbs (u, v) -> "(" ^ string_of_ty u ^ " => " ^ string_of_ty v ^ ")"
+  | TProd (u, v) -> "(" ^ string_of_ty u ^ " /\\ " ^ string_of_ty v ^ ")"
 
 let rec string_of_tm t =
   match t with
@@ -22,6 +30,9 @@ let rec string_of_tm t =
   | App (u, v) -> string_of_tm u ^ " " ^ string_of_tm v
   | Abs (x, tx, u) ->
       "(fun (" ^ x ^ " : " ^ string_of_ty tx ^ ") -> " ^ string_of_tm u ^ ")"
+  | Prod (u, v) -> "(" ^ string_of_tm u ^ ", " ^ string_of_tm v ^ ")"
+  | Fst u -> "fst (" ^ string_of_tm u ^ ")"
+  | Snd u -> "snd (" ^ string_of_tm u ^ ")"
 
 type context = (var * ty) list
 
@@ -33,10 +44,19 @@ let rec infer_type' gamma t =
   | App (u, v) -> (
       let tu = infer_type' gamma u in
       let tv = infer_type' gamma v in
-      match tu with Arr (ta, tb) when ta = tv -> tb | _ -> raise Type_error)
+      match tu with TAbs (ta, tb) when ta = tv -> tb | _ -> raise Type_error)
   | Abs (x, tx, u) ->
       let tu = infer_type' ((x, tx) :: gamma) u in
-      Arr (tx, tu)
+      TAbs (tx, tu)
+  | Prod (u, v) -> TProd (infer_type' gamma u, infer_type' gamma v)
+  | Fst u -> (
+      match infer_type' gamma u with
+      | TProd (tu, _) -> tu
+      | _ -> raise Type_error)
+  | Snd u -> (
+      match infer_type' gamma u with
+      | TProd (_, tu) -> tu
+      | _ -> raise Type_error)
 
 let check_type' gamma t ty =
   match infer_type' gamma t with tt when tt = ty -> () | _ -> raise Type_error
@@ -48,7 +68,7 @@ let check_type' gamma t ty =
 (*********)
 
 let () =
-  let test_type = Arr (Arr (TVar "A", TVar "B"), Arr (TVar "A", TVar "C")) in
+  let test_type = TAbs (TAbs (TVar "A", TVar "B"), TAbs (TVar "A", TVar "C")) in
   let s = string_of_ty test_type in
   print_endline s
 
@@ -56,7 +76,7 @@ let () =
   let test_term =
     Abs
       ( "f",
-        Arr (TVar "A", TVar "B"),
+        TAbs (TVar "A", TVar "B"),
         Abs ("x", TVar "A", App (Var "f", Var "x")) )
   in
   let s = string_of_tm test_term in
@@ -66,16 +86,16 @@ let () =
   let t =
     Abs
       ( "f",
-        Arr (TVar "A", TVar "B"),
+        TAbs (TVar "A", TVar "B"),
         Abs
           ( "g",
-            Arr (TVar "B", TVar "C"),
+            TAbs (TVar "B", TVar "C"),
             Abs ("x", TVar "A", App (Var "g", App (Var "f", Var "x"))) ) )
   in
   let ty =
-    Arr
-      ( Arr (TVar "A", TVar "B"),
-        Arr (Arr (TVar "B", TVar "C"), Arr (TVar "A", TVar "C")) )
+    TAbs
+      ( TAbs (TVar "A", TVar "B"),
+        TAbs (TAbs (TVar "B", TVar "C"), TAbs (TVar "A", TVar "C")) )
   in
   assert (infer_type' [] t = ty)
 
@@ -97,7 +117,7 @@ let () =
   let t =
     Abs
       ( "f",
-        Arr (TVar "A", TVar "B"),
+        TAbs (TVar "A", TVar "B"),
         Abs ("x", TVar "B", App (Var "f", Var "x")) )
   in
   try
@@ -107,12 +127,12 @@ let () =
 
 let () =
   let t = Abs ("x", TVar "A", Var "x") in
-  let ty = Arr (TVar "A", TVar "A") in
+  let ty = TAbs (TVar "A", TVar "A") in
   check_type' [] t ty
 
 let () =
   let t = Abs ("x", TVar "A", Var "x") in
-  let ty = Arr (TVar "B", TVar "B") in
+  let ty = TAbs (TVar "B", TVar "B") in
   try
     let _ = check_type' [] t ty in
     assert false
